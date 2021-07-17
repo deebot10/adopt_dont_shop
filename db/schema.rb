b77c6c5a5a04514c36ac9027de792f6c0669c298
deebot10 <dee1jhill@gmail.com>
2021-07-17 16:14:44 -0600

    Create meigration to add description to applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_211737) do
+ActiveRecord::Schema.define(version: 2021_07_17_221254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_07_17_211737) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "pet_applications", force: :cascade do |t|
