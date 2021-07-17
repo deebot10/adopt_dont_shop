dbfad2dd0301f89b44cbb52e837b7ce0e0a73ef6
deebot10 <dee1jhill@gmail.com>
2021-07-17 16:53:20 -0600

    Create test for user story 2

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -81,4 +81,42 @@ RSpec.describe 'the pets index' do
     expect(page).to have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
   end
+  
+    #   As a visitor
+    # When I visit the pet index page
+    # Then I see a link to "Start an Application"
+    # When I click this link
+    # Then I am taken to the new application page where I see a form
+    # When I fill in this form with my:
+
+    # Name
+    # Street Address
+    # City
+    # State
+    # Zip Code
+    # And I click submit
+    # Then I am taken to the new application's show page
+    # And I see my Name, address information, and description of why I would make a good home
+    # And I see an indicator that this application is "In Progress"
+  it 'has a link to create applications' do
+
+    visit '/pets'
+    
+    click_button 'Start an Application'
+
+    expect(current_path).to eq('/applications/new')
+
+    fill_in 'Name',     with: 'Dee'
+    fill_in 'Address',  with: '124 Oak St.'
+    fill_in 'City',     with: 'Dallas'
+    fill_in 'State' ,   with: 'Tx'
+    fill_in 'Zip Code', with: 27854
+    click_on("Submit")
+    
+    applicant = Application.last
+
+    expect(current_path).to eq("/applications/#{applicant.id}")
+
+    expect(page).to have_content('Dee')
+  end
 end
