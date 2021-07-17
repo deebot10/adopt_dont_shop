457c50ceab29bb8a4434eb29782c7f1c6b9fdad2
deebot10 <dee1jhill@gmail.com>
2021-07-17 16:42:25 -0600

    Pass user story1

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Application Show' do
   before(:each) do
-    @applicant = Application.create!(name: 'Dee', address: '123 Oak St.', city: 'Austin', state: 'Tx', zip_code: 13546, description: "I'd be a ok owner") 
+    @applicant = Application.create!(name: 'Dee', address: '123 Oak St.', city: 'Austin', state: 'Tx', zip_code: 13546, description: "I'd be a ok owner", status: 'Pending') 
     @shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9) 
     @pet_1 = @applicant.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter.id)
     @pet_2 = @applicant.pets.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter.id)
@@ -10,15 +10,7 @@ RSpec.describe 'Application Show' do
     visit application_path(@applicant)
   end
   
-    #   As a visitor
-    # When I visit an applications show page
-    # Then I can see the following:
-
-    # Name of the Applicant
-    # Full Address of the Applicant including street address, city, state, and zip code
-    # Description of why the applicant says they'd be a good home for this pet(s)
-    # names of all pets that this application is for (all names of pets should be links to their show page)
-    # The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
+  # User Story1
   it 'shows an Application with attributes' do
 
     expect(page).to have_content(@applicant.name)
@@ -27,5 +19,16 @@ RSpec.describe 'Application Show' do
     expect(page).to have_content(@applicant.state)
     expect(page).to have_content(@applicant.zip_code)
     expect(page).to have_content(@applicant.description)
+    expect(page).to have_content(@applicant.status)
+  end
+
+  it 'names all the pets for an applicant and links to show page' do
+    
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to have_content(@pet_2.name)
+
+    click_link("#{@pet_1.name}")
+
+    expect(current_path).to eq("/pets/#{@pet_1.id}")
   end
 end
