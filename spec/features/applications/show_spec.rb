13b049092a80784332d074d74b14b80770bad535
deebot10 <dee1jhill@gmail.com>
2021-07-18 15:44:54 -0600

    Write test for admin index

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -31,4 +31,17 @@ RSpec.describe 'Application Show' do
 
     expect(current_path).to eq("/pets/#{@pet_1.id}")
   end
+  
+  #   As a visitor UserStory3
+  # When I visit an application's show page
+  # And that application has not been submitted,
+  # Then I see a section on the page to "Add a Pet to this Application"
+  # In that section I see an input where I can search for Pets by name
+  # When I fill in this field with a Pet's name
+  # And I click submit,
+  # Then I am taken back to the application show page
+  # And under the search bar I see any Pet whose name matches my search
+  xit 'has a section to add a pet to an application' do
+      
+  end
 end
--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -82,22 +82,7 @@ RSpec.describe 'the pets index' do
     expect(page).to_not have_content(pet_3.name)
   end
   
-    #   As a visitor
-    # When I visit the pet index page
-    # Then I see a link to "Start an Application"
-    # When I click this link
-    # Then I am taken to the new application page where I see a form
-    # When I fill in this form with my:
-
-    # Name
-    # Street Address
-    # City
-    # State
-    # Zip Code
-    # And I click submit
-    # Then I am taken to the new application's show page
-    # And I see my Name, address information, and description of why I would make a good home
-    # And I see an indicator that this application is "In Progress"
+  #User Story2 :in progress
   it 'has a link to create applications' do
 
     visit '/pets'
