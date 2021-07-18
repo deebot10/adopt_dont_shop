f58da0e1f43e13bc5c52e26df82995c04378b64c
deebot10 <dee1jhill@gmail.com>
2021-07-17 18:16:31 -0600

    Create new and create method wwthin AppController

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,11 @@ class ApplicationsController < ApplicationController
   def show
     @applicant = Application.find(params[:id])    
   end    
+
+  def new;end
+
+  def create
+    applicant = Application.create!(applicant_params)
+    redirect_to "/applications/#{applicant.id}"     
+  end
 end
